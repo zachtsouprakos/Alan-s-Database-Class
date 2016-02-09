1d4ad77ace818aaf55f30b7a9dc0358d3da583b6
zachtsouprakos <zachary.tsouprakos1@marist.edu>
2016-02-09 11:28:00 -0500

    Couple of comment changes
    I asked myself, “what would Alan do?”… had to make some changes as you
    were telling us too

--- Lab 3 statements.sql
+++ Lab 3 statements.sql
@@ -1,43 +1,43 @@
-#Selects ordnum and totalUSD from orders
+-- #1 Selects ordnum and totalUSD from orders
 Select ordnum, totalusd 
 from orders ;
 
-#Selects names and cities from agents where the name is equal to Smith
+-- #2 Selects names and cities from agents where the name is equal to Smith
 SELECT name, city, 
 FROM agents
 WHERE name = 'Smith' ;
 
-#Selects Pid, name, and priceUSD from products where quantity is greater than 20800
+-- #3 Selects Pid, name, and priceUSD from products where quantity is greater than 20800
 SELECT pid, name, priceUSD
 FROM products
 WHERE quantity > 208000 ;
 
-#Selects names and cities from customers where city is equal to Dallas
+-- #4 Selects names and cities from customers where city is equal to Dallas
 SELECT name, city 
 FROM customers
 WHERE city = 'Dallas' ;
 
-#used '<>' to set city not equal to New York and Tokyo
+-- #5 used '<>' to set city not equal to New York and Tokyo
 SELECT name 
 FROM agents 
 WHERE city <> 'New York' AND city <> 'Tokyo' ;
 
-#Used '>=' to set priceUSD to be greater than or equal to 1
+-- #6 Used '>=' to set priceUSD to be greater than or equal to 1
 SELECT *
 FROM products
 WHERE <> 'Dallas' AND city <> 'Duluth' AND priceUSD >= 1 ;
 
-#Used like for jan and mar due to its data type
+-- #7 Used like for jan and mar due to its data type
 SELECT * 
 FROM orders
 WHERE mon like 'jan' OR mon like 'mar' ;
 
-#Selects all data from orders where month is 'feb' and totalUSD is less than 500
+-- #8 Selects all data from orders where month is 'feb' and totalUSD is less than 500
 SELECT * 
 FROM orders
 WHERE mon like 'feb' AND totalusd < 500 ;
 
-#Selects all data from customers where cid is 'c005'
+-- #9 Selects all data from customers where cid is 'c005'
 SELECT *
 FROM customers
 WHERE cid like 'c005' ;
