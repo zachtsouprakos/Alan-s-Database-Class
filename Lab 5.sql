0551a1c3c193c5fa77f4f86423c3b8d4f7df81f0
zachtsouprakos <zachary.tsouprakos1@marist.edu>
2016-02-22 21:57:37 -0500

    lab 5, Zachary Tsouprakos, 2/22/2016
    Lab 5.. number 7 is the hardest SQL statement ever

---
+++ Lab 5.sql
@@ -0,0 +1,39 @@
+-- Zachary Tsouprakos, Lab 5
+
+-- #1 This statement selects the city from the agent who places an order for customer c002
+SELECT city
+FROM orders o, agents a 
+WHERE o.aid = a.aid
+and o.cid = 'c002' ;
+
+--#2 This statement selects the product ID from any agent where the customers city is Dallas
+SELECT o.pid
+FROM orders o, customers c
+WHERE o.cid = c.cid
+AND c.city = 'Dallas'
+order by o.pid dESC ;
+
+--#3 Selects the customer who did not place an order
+SELECT DISTINCT c.name
+FROM customers c, orders o
+WHERE c.cid NOT IN (SELECT cid 
+                    from orders  
+                  ) 
+;
+
+--#4 Selects the customers name from orders where there is a CID in customers but not orders
+SELECT c.name
+FROM customers c FULL OUTER JOIN orders o ON c.cid = o.cid
+WHERE o.cid is NULL ;
+
+--#5 Selects the customers and agents name that are from the same city and placed at least one order with that agent
+SELECT DISTINCT c.name, a.name
+FROM orders o INNER JOIN agents a ON o.aid = a.aid
+              INNER JOIN customers c ON o.cid = c.cid
+WHERE a.city = c.city ;
+
+--#6 This statement selects the name and city of agents who live in the same city
+SELECT c.name, a.name, c.city, a.city
+FROM customers c INNER JOIN agents a ON c.city = a.city ;
+
+--#7Select the name and city of customers who live in the same city the produce the fewest variety of products
