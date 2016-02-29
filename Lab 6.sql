61c9e7bc260a10c5250a60806023c4ff5d5bf360
zachtsouprakos <zachary.tsouprakos1@marist.edu>
2016-02-29 12:18:21 -0500

    updated answers
    figured out number 6 and updated my number 4

--- Lab 6.sql
+++ Lab 6.sql
@@ -12,19 +12,19 @@ WHERE c.city IN (SELECT city
  -- #2 This statement selects the name of the product that has a greater 'priceUSD' than the avg of priceUSD
 SELECT name 
 FROM products p
-WHERE p.priceUSD > (SELECt AVG(priceUSD) as avgpriceUSD
+WHERE p.priceUSD > (SELECt AVG(priceUSD) AS avgpriceUSD
                      FROM products
                     )
 ORDER BY NAME DESC ;
 
 -- #3 Displays the customers name, the product ID(pid), and the total USD orders by totalUSD from high to low
 SELECT c.name, o.pid, o.totalUSD
-FROM customers c inner join orders o on c.cid = o.cid
+FROM customers c INNER JOIN orders o ON c.cid = o.cid
 order by totalUSD DESC ;
 
---#4 NOT DONE FIX THIS ONE!!!!!!!!!******
+--#4 This statement does not use coalesce but returns users name and totalUSD
 SELECT DISTINCT c.name, o.totalUSD
-FROM customers c inner join orders o ON c.cid = o.cid
+FROM customers c INNER JOIN orders o ON c.cid = o.cid
 ORDER BY name ;
 
 --#5 This statement selects the customers name, product name, and agents, where the customer placed an order 
@@ -35,4 +35,22 @@ FROM orders o INNER JOIN customers c ON o.cid = c.cid
               INNER JOIN agents a ON o.aid = a.aid
 WHERE a.city = 'Tokyo' ;
 
---#6 
+--#6Created a view of the correct prices. Then compared this view to the unchecked prices using a subquery with an except
+-- statement. Alex Millet and I collaborated to come to this conclusion... it is pretty awesome.
+CREATE OR replace view PRICECHECK
+AS
+SELECT ((p.priceusd*o.qty)-(((c.discount/100)*p.priceusd)*o.qty)) AS priceCHECK, o.ordnum
+FROM orders o INNER join customers c ON o.cid = c.cid
+              INNER join products p ON o.pid = p.pid
+
+SELECT ordnum, priceCHECK
+FROM PRICECHECK
+WHERE priceCHECK IN (SELECT priceCHECK
+                  FROM PRICECHECK 
+                  EXCEPT
+                  SELECT totalusd
+                  FROM orders
+                 ) ;
+
+
+
